0140403062ec89872453b9e8244c2caba1ae63d2
LNBQIAN <geniusiswo@vip.qq.com>
2018-05-25 11:10:09 +0800

    525

---
+++ aspnet-core/src/Scorpio.Core/PhoneBooks/Persons/Person.cs
@@ -0,0 +1,13 @@
+﻿using Abp.Domain.Entities;
+using Abp.Domain.Entities.Auditing;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Scorpio.PhoneBooks.Persons
+{
+    public class Person: FullAuditedEntity //Entity<Guid> ,
+    {
+        public string Name { get; set; }
+    }
+}
